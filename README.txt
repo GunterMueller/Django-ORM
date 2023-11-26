1ae7bcada7221f1d39fcc5adf773f57ddd2eeb5b
Gunter Mueller <gunter.mueller@gmail.com>
2023-11-26 15:54:29 -0500

    Added writing to csv file.

---
+++ README.txt
@@ -0,0 +1,8 @@
+in the virtual env install
+pip install six
+pip install django-queryset-csv
+
+
+Then run VS Code in the venv:
+cd  <top level dir of venv directory>
+code .
--- main.py
+++ main.py
@@ -6,12 +6,20 @@ these settings as is, and skip to START OF APPLICATION section below """
 
 # Turn off bytecode generation
 import sys
+import csv
+from django.http import HttpResponse
+
 sys.dont_write_bytecode = True
+
+filePath = '/home/gunter/GM_Programming/PLAY_GROUNDS/Python/sqlite3_orm/'
+fileName = 'orm_export.csv'
 
 # Django specific settings
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 import django
+from djqscsv import write_csv
+
 django.setup()
 
 # Import your models for use in your script
@@ -26,5 +34,16 @@ from db.models import *
 User.objects.create(name='Dan')
 User.objects.create(name='Robert')
 
+model_class = User
+
+meta = model_class._meta
+field_names = [field.name for field in meta.fields]
+
 for u in User.objects.all():
     print(f'ID: {u.id} \tUsername: {u.name}')
+
+
+qs = User.objects.all()
+with open(filePath + fileName, 'wb') as csv_file:
+  write_csv(qs, csv_file)
+
